637997caf1dc9ce70385abe252044005ae5bdedb
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-07 09:49:33 -0500

    part 2

--- 2024/problem_07.py
+++ 2024/problem_07.py
@@ -1,5 +1,4 @@
 import sys
-from functools import reduce
 from itertools import product
 
 
@@ -10,8 +9,11 @@ def parse_line(line: str) -> tuple[int, list[int]]:
     return result, inputs
 
 
-def is_possible(result: int, inputs: list[int]) -> bool:
-    for operator_comb in list(product(['*', '+'], repeat=len(inputs) - 1)):
+def is_possible(result: int, inputs: list[int], operators: list[str], required: str | None = None) -> bool:
+    operator_combs = list(product(operators, repeat=len(inputs) - 1))
+    if required:
+        operator_combs = [o for o in operator_combs if required in o]
+    for operator_comb in operator_combs:
         if eval_statement(inputs, operator_comb) == result:
             return True
     return False
@@ -27,11 +29,14 @@ def eval_statement(inputs: list[int], operators: list[str]) -> str:
 if __name__ == "__main__":
     input_path = sys.argv[1]
     part1 = 0
+    part2 = 0
     with open(input_path, 'r') as f:
         for line in f.readlines():
             result, inputs = parse_line(line.strip())
-            if is_possible(result, inputs):
+            if is_possible(result, inputs, operators=['*', '+']):
                 part1 += result
-    part2 = 0
+                part2 += result
+            elif is_possible(result, inputs, operators=['*', '+', ''], required=''):
+                part2 += result
     print(f'Part 1: {part1}')
     print(f'Part 2: {part2}')
