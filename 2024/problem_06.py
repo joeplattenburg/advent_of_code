9743682253d967e23225ceb04a7eab70460519db
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-06 19:10:12 -0500

    a little better

--- 2024/problem_06.py
+++ 2024/problem_06.py
@@ -3,10 +3,6 @@ import sys
 import numpy as np
 
 
-class LoopError(Exception):
-    pass
-
-
 class Grid:
     directions = ['^', '>', 'v', '<']
 
@@ -16,8 +12,11 @@ class Grid:
         self.direction: str = str(grid[starting_loc_ind][0])
         x, y = np.where(starting_loc_ind)
         self.loc = int(x[0]), int(y[0])
+        self.history = [(self.loc, self.direction)]
         self.grid = grid
+        self.original_grid = deepcopy(grid)
         self.loc_on_grid = True
+        self.loop = False
 
     def take_step(self) -> None:
         if self.direction == '^':
@@ -37,6 +36,9 @@ class Grid:
                 self.grid[*self.loc] = 'X'
                 self.loc = test_loc
             self.grid[*self.loc] = self.direction
+            if (self.loc, self.direction) in self.history:
+                self.loop = True
+            self.history.append((self.loc, self.direction))
         else:
             self.grid[*self.loc] = 'X'
             self.loc_on_grid = False
@@ -55,41 +57,36 @@ class Grid:
     def count_locations(self) -> int:
         return int((self.grid == 'X').sum())
 
-    def open_locations(self) -> list[tuple[int, int]]:
-        return [tuple(t) for t in np.argwhere(self.grid == '.').tolist()]
-
-    def walk(self, cutoff=100000):
-        counter = 0
-        while self.loc_on_grid:
+    def walk(self):
+        while self.loc_on_grid and not self.loop:
             self.take_step()
-            counter += 1
-            if counter >= cutoff:
-                raise LoopError
+
+    def reset(self):
+        self.loc, self.direction = self.history[0]
+        self.history = [(self.loc, self.direction)]
+        self.grid = deepcopy(self.original_grid)
+        self.loc_on_grid = True
+        self.loop = False
 
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
-    original_array = []
+    array = []
     with open(input_path, 'r') as f:
         for line in f.readlines():
-            original_array.append([c for c in line.strip()])
-    original_array = np.array(original_array)
-    grid = Grid(deepcopy(original_array))
+            array.append([c for c in line.strip()])
+    grid = Grid(np.array(array))
     grid.walk()
     part1 = grid.count_locations()
-    print('Part 1 done')
 
     part2 = 0
-    grid = Grid(original_array)
-    open_locs = grid.open_locations()
-    for i, loc in enumerate(grid.open_locations()):
-        print(f'Testing loc {i + 1} of {len(open_locs)}...')
-        new_array = deepcopy(original_array)
-        new_array[*loc] = '#'
-        new_grid = Grid(new_array)
-        try:
-            new_grid.walk(cutoff=100000)
-        except LoopError:
+    test_locs = [tuple(t) for t in np.argwhere(grid.grid == 'X').tolist()]
+    for i, loc in enumerate(test_locs):
+        print(f'Testing loc {i + 1} of {len(test_locs)}...')
+        grid.reset()
+        grid.grid[*loc] = '#'
+        grid.walk()
+        if grid.loop:
             part2 += 1
     print(f'Part 1: {part1}')
     print(f'Part 2: {part2}')
