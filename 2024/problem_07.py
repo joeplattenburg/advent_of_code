abf08928c23cf607f8ba4f4e9c9607d45389633f
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-07 09:22:59 -0500

    day 7 part 1

---
+++ 2024/problem_07.py
@@ -0,0 +1,37 @@
+import sys
+from functools import reduce
+from itertools import product
+
+
+def parse_line(line: str) -> tuple[int, list[int]]:
+    left, right = line.split(':')
+    result = int(left.strip())
+    inputs = [int(r) for r in right.strip().split()]
+    return result, inputs
+
+
+def is_possible(result: int, inputs: list[int]) -> bool:
+    for operator_comb in list(product(['*', '+'], repeat=len(inputs) - 1)):
+        if eval_statement(inputs, operator_comb) == result:
+            return True
+    return False
+
+
+def eval_statement(inputs: list[int], operators: list[str]) -> str:
+    value = inputs[0]
+    for operator, next_value in zip(operators, inputs[1:]):
+        value = eval(str(value) + operator + str(next_value))
+    return value
+
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    part1 = 0
+    with open(input_path, 'r') as f:
+        for line in f.readlines():
+            result, inputs = parse_line(line.strip())
+            if is_possible(result, inputs):
+                part1 += result
+    part2 = 0
+    print(f'Part 1: {part1}')
+    print(f'Part 2: {part2}')
