04d4e40598edd56aa7c7eb421fb8f7c9b1e4ded2
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-12 21:58:29 -0500

    day 12

--- 2024/problem_12.py
+++ 2024/problem_12.py
@@ -1,30 +1,44 @@
 import sys
+from collections import Counter, defaultdict
 import numpy as np
 from scipy.ndimage import label
 
 
-def get_perimeter(inds: list[tuple[int, int]], grid: np.ndarray) -> int:
-    out = 0
+def get_all_sides(inds: list[tuple[int, int]], grid: np.ndarray) -> list[tuple[tuple[int, int], complex]]:
+    edges = []
     for ind in inds:
         self = grid[*ind]
-        for neighbor_ind in get_neighbor_inds(*ind):
-            try:
-                neighbor = grid[*neighbor_ind]
-                out += int(neighbor != self)
-            except IndexError:
-                out += 1
-    return out
+        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            neighbor_ind = ind[0] + di, ind[1] + dj
+            if not (on_grid(neighbor_ind, grid) and grid[*neighbor_ind] == self):
+                edges.append((tuple(ind), complex(di, dj)))
+    return edges
 
 
-def get_neighbor_inds(i: int, j: int) -> list[tuple[int, int]]:
-    return [(i + di, j + dj) for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
+def get_perimeter(inds: list[tuple[int, int]], grid: np.ndarray) -> int:
+    return len(get_all_sides(inds, grid))
 
 
-def split_locs(regions: list[str], grid: np.ndarray):
+def get_sides(inds: list[tuple[int, int]], grid: np.ndarray) -> int:
+    all_sides = get_all_sides(inds, grid)
+    sides = defaultdict(list)
+    for (si, sj), direc in all_sides:
+        if direc.real != 0:
+            sides[(si, direc)].append(sj)
+        else:
+            sides[(sj, direc)].append(si)
+    return sum(sum(np.diff(np.array(sorted(s))) > 1) + 1 for s in sides.values())
+
+
+def on_grid(ind: tuple[int, int], grid: np.ndarray) -> bool:
+    return (0 <= ind[0] < grid.shape[0]) and (0 <= ind[1] < grid.shape[1])
+
+
+def get_regions(grid: np.ndarray) -> dict[tuple[str, int], list[tuple[int, int]]]:
+    regions = np.unique(grid)
     out = {}
     for r in regions:
         inds, n_regions = label(grid == r)
-        #print(inds)
         for rr in range(n_regions):
             out[(r, rr)] = np.argwhere(inds == (rr + 1)).tolist()
     return out
@@ -37,13 +51,10 @@ if __name__ == "__main__":
         for line in f.readlines():
             grid.append([c for c in line.strip()])
     grid = np.array(grid)
-    #locs = {region: np.argwhere(grid == region).tolist() for region in np.unique(grid)}
-    locs = split_locs(np.unique(grid), grid)
-    part1 = 0
-    for (region, _), inds in locs.items():
-        score = len(inds) * get_perimeter(inds, grid)
-        #print(region, score)
-        part1 += score
-    part2 = 0
+    locs = get_regions(grid)
+    part1, part2 = 0, 0
+    for (region, i), inds in locs.items():
+        part1 += len(inds) * get_perimeter(inds, grid)
+        part2 += len(inds) * get_sides(inds, grid)
     print(f'Part 1: {part1}')
     print(f'Part 2: {part2}')
