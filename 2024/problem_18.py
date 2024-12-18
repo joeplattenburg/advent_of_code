c291aacada84baf270604c7bde20ccb62cccc226
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-18 08:45:49 -0500

    day 18 part 2

--- 2024/problem_18.py
+++ 2024/problem_18.py
@@ -1,16 +1,9 @@
 import sys
 import networkx as nx
+from networkx.exception import NetworkXNoPath
 
 
-if __name__ == "__main__":
-    input_path = sys.argv[1]
-    memory_space, n_bytes = (7, 12) if input_path.endswith('0.txt') else (71, 1024)
-    coords = []
-    with open(input_path, 'r') as f:
-        for line in f.readlines():
-            i, j = line.strip().split(',')
-            coords.append((int(i), int(j)))
-    corrupted = set(coords[:n_bytes])
+def build_graph(memory_space: int, corrupted: set[tuple[int, int]]) -> nx.Graph:
     nodes = [
         (i, j)
         for i in range(memory_space)
@@ -30,7 +23,32 @@ if __name__ == "__main__":
     g = nx.Graph()
     g.add_nodes_from(nodes)
     g.add_edges_from(edges)
-    path = nx.shortest_path(g, source=(0, 0), target=(memory_space - 1, memory_space - 1))
+    return g
+
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    memory_space, n_bytes = (7, 12) if input_path.endswith('0.txt') else (71, 1024)
+    coords = []
+    with open(input_path, 'r') as f:
+        for line in f.readlines():
+            i, j = line.strip().split(',')
+            coords.append((int(i), int(j)))
+    corrupted = set(coords[:n_bytes])
+    source, target = (0, 0), (memory_space - 1, memory_space - 1)
+    g = build_graph(memory_space, corrupted)
+    path = nx.shortest_path(g, source, target)
     part1 = len(path) - 1
+
+    blocked = False
+    while not blocked:
+        g.remove_node(coords[n_bytes])
+        try:
+            path = nx.shortest_path(g, source, target)
+        except NetworkXNoPath:
+            part2 = coords[n_bytes]
+            blocked = True
+        n_bytes += 1
+
     print(f'Part 1: {part1}')
-    #print(f'Part 2: {part2}')
+    print(f'Part 2: {part2}')
