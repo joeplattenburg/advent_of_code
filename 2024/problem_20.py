875cf93902e5d93c1787af0a72e91dea8b4d4f23
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-20 18:30:53 -0500

    day 20 part 2

--- 2024/problem_20.py
+++ 2024/problem_20.py
@@ -28,15 +28,25 @@ def get_edges(new_nodes: set[Node], all_nodes: set[Node], shape: tuple[int, int]
         )
     )
 
-def get_neighbors_within_n(node: Node, n: int = 2) -> set[Node]:
-    return set(
-        (node[0] + di, node[1] + dj)
-        for di, dj in (
-            (0, 1), (0, 2), (0, -1), (0, -2),
-            (1, 0), (2, 0), (-1, 0), (-2, 0),
-            (1, 1), (1, -1), (-1, 1), (-1, -1),
-        )
-    )
+
+def get_neighbors_within_n(node: Node, n: int) -> dict[Node, int]:
+    return {
+        (node[0] + di, node[1] + dj): abs(di) + abs(dj)
+        for di in range(-n, n + 1)
+        for dj in range(-n, n + 1)
+        if 0 < abs(di) + abs(dj) <= n
+    }
+
+
+def count_cheats(cumulative_time: dict[Node, int], duration: int, better_than: int = 0) -> int:
+    cheats = {}
+    for node, time in cumulative_time.items():
+        neighbors = get_neighbors_within_n(node, n=duration)
+        for neighbor, dist in neighbors.items():
+            if (saved_time := cumulative_time.get(neighbor, 0) - time - dist) > 0:
+                cheats[(node, neighbor)] = saved_time
+    cheat_summary = Counter(cheats.values())
+    return sum(v for k, v in cheat_summary.items() if k >= better_than)
 
 
 if __name__ == "__main__":
@@ -51,14 +61,7 @@ if __name__ == "__main__":
     g.add_edges_from(edges)
     fair_path = nx.shortest_path(g, start_node, end_node)
     cumulative_time = {node: i for i, node in enumerate(fair_path)}
-    cheats = {}
-    for node, time in cumulative_time.items():
-        neighbors = get_neighbors_within_n(node)
-        for neighbor in neighbors:
-            if (subpath := cumulative_time.get(neighbor, 0) - time) > 2:
-                cheats[(node, neighbor)] = subpath - 2
-    cheat_summary = Counter(cheats.values())
-    part1 = sum(v for k, v in cheat_summary.items() if k >= 100)
-    part2 = 0
+    part1 = count_cheats(cumulative_time, duration=2, better_than=100)
+    part2 = count_cheats(cumulative_time, duration=20, better_than=100)
     print(f'Part 1: {part1}')
     print(f'Part 2: {part2}')
