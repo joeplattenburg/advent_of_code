3aff8842053876365f43cd4afffb32c13b5845a9
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-20 17:58:51 -0500

    refactor in prep for part 2

--- 2024/problem_20.py
+++ 2024/problem_20.py
@@ -1,5 +1,4 @@
 import sys
-from copy import deepcopy
 from collections import Counter
 
 import networkx as nx
@@ -29,6 +28,16 @@ def get_edges(new_nodes: set[Node], all_nodes: set[Node], shape: tuple[int, int]
         )
     )
 
+def get_neighbors_within_n(node: Node, n: int = 2) -> set[Node]:
+    return set(
+        (node[0] + di, node[1] + dj)
+        for di, dj in (
+            (0, 1), (0, 2), (0, -1), (0, -2),
+            (1, 0), (2, 0), (-1, 0), (-2, 0),
+            (1, 1), (1, -1), (-1, 1), (-1, -1),
+        )
+    )
+
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
@@ -36,33 +45,19 @@ if __name__ == "__main__":
     start_node, end_node = tuple(np.argwhere(maze == 'S').tolist()[0]), tuple(np.argwhere(maze == 'E').tolist()[0])
     inter = [(int(i), int(j)) for i, j in np.argwhere(maze == '.')]
     all_nodes = {start_node, *inter, end_node}
-    walls = set((int(i), int(j)) for i, j in np.argwhere(maze == '#'))
     edges = get_edges(new_nodes=all_nodes, all_nodes=all_nodes, shape=maze.shape)
     g = nx.Graph()
     g.add_nodes_from(all_nodes)
     g.add_edges_from(edges)
     fair_path = nx.shortest_path(g, start_node, end_node)
-    fair_time = len(fair_path) - 1
-    cheat_locs = set(
-        (i + di, j + dj)
-        for i, j in fair_path
-        for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0))
-        if (
-            (i + di, j + dj) in walls
-            and 0 < (i + di) < (maze.shape[0] - 1)
-            and 0 < (j + dj) < (maze.shape[1] - 1)
-        )
-    )
-    cheat_times = {}
-    for i, cheat in enumerate(cheat_locs):
-        print(f'Testing {i} of {len(cheat_locs)}...')
-        g_test = deepcopy(g)
-        new_edges = get_edges(new_nodes={cheat}, all_nodes=all_nodes, shape=maze.shape)
-        if len(new_edges) > 1:
-            g_test.add_edges_from(new_edges)
-            cheat_times[cheat] = fair_time - (len(nx.shortest_path(g_test, start_node, end_node)) - 1)
-    cheat_summary = Counter(cheat_times.values())
-    print(cheat_summary)
+    cumulative_time = {node: i for i, node in enumerate(fair_path)}
+    cheats = {}
+    for node, time in cumulative_time.items():
+        neighbors = get_neighbors_within_n(node)
+        for neighbor in neighbors:
+            if (subpath := cumulative_time.get(neighbor, 0) - time) > 2:
+                cheats[(node, neighbor)] = subpath - 2
+    cheat_summary = Counter(cheats.values())
     part1 = sum(v for k, v in cheat_summary.items() if k >= 100)
     part2 = 0
     print(f'Part 1: {part1}')
