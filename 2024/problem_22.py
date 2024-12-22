052a48f46ac0b99a93941545216487513bb1166f
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-22 10:51:40 -0500

    day 22 part 2

--- 2024/problem_22.py
+++ 2024/problem_22.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def parse_input(input_path) -> list[int]:
@@ -14,15 +15,33 @@ def next_number(n: int) -> int:
     return n
 
 
+def cycle_diffs(d: list[int], new: int):
+    d.append(new)
+    if len(d) > 4:
+        d.pop(0)
+
+
 if __name__ == "__main__":
     input_path = sys.argv[1]
     secrets = parse_input(input_path)
+    n_rounds = 2000
     part1 = 0
-    for i, secret in enumerate(secrets):
-        for _ in range(2000):
+    counter = defaultdict(int)
+    for secret in secrets:
+        price = int(str(secret)[-1])
+        already_seen = set()
+        diffs = []
+        for i in range(n_rounds):
             secret = next_number(secret)
-        print(f'Final value: {secret}')
+            new_price = int(str(secret)[-1])
+            cycle_diffs(diffs, new_price - price)
+            if len(diffs) == 4:
+                seq = tuple(diffs)
+                if seq not in already_seen:
+                    already_seen.add(seq)
+                    counter[seq] += new_price
+            price = new_price
         part1 += secret
-    part2 = 0
+    part2 = max(counter.values())
     print(f'Part 1: {part1}')
     print(f'Part 2: {part2}')
