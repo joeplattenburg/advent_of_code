a5a01b1a36b8cfa14e8a5a005808d7e208ec7d58
Joe Plattenburg <joeplattenburg@gmail.com>
2024-12-25 10:10:19 -0500

    day 25

---
+++ 2024/problem_25.py
@@ -0,0 +1,23 @@
+import sys
+import numpy as np
+
+
+def parse_input(input_path) -> tuple[list[np.ndarray], list[np.ndarray], int]:
+    locks, keys = [], []
+    with open(input_path, 'r') as f:
+        grids = f.read().split('\n\n')
+    for grid in grids:
+        array = np.array([[c for c in line] for line in grid.split('\n')])
+        space = array.shape[0] - 2
+        if all(array[0, :] == '#'):
+            locks.append((array == '#').sum(axis=0) - 1)
+        else:
+            keys.append((array == '#').sum(axis=0) - 1)
+    return locks, keys, space
+
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    locks, keys, space = parse_input(input_path)
+    part1 = sum(all((lock + key) <= space) for key in keys for lock in locks)
+    print(f'Part 1: {part1}')
