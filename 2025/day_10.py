bec0bf28cb2ba024468feb4fc47537bcf6b9f4ec
Joe Plattenburg <joeplattenburg@gmail.com>
2025-12-29 00:18:56 -0500

    day 10 part 2

--- 2025/day_10.py
+++ 2025/day_10.py
@@ -1,6 +1,8 @@
 import sys
 from typing import TypeVar
 from itertools import chain, combinations
+from scipy.optimize import linprog
+import numpy as np
 
 A = TypeVar('A')
 
@@ -30,15 +32,29 @@ def press_buttons(buttons: set[tuple[int, ...]], n: int) -> str:
     return ''.join(str(i) for i in lights)
 
 
+def solve_program(buttons: list[tuple[int, ...]], joltage: list[int]) -> list[int]:
+    c = np.ones((len(buttons), ))
+    A = []
+    for b in buttons:
+        temp = np.zeros((len(joltage), ))
+        temp[list(b)] = 1.
+        A.append(temp)
+    A = np.array(A).transpose()
+    b = np.array(joltage)
+    r = linprog(c=c, A_eq=A, b_eq=b, integrality=1)
+    return [round(x) for x in r['x']]
+
+
 if __name__ == "__main__":
     input_path = sys.argv[1]
     part1, part2 = 0, 0
     with open(input_path, 'r') as f:
-        for line in f.readlines():
+        for i, line in enumerate(f.readlines()):
             lights, buttons, joltage = parse_line(line.strip())
             for b in powerset(set(buttons)):
                 if press_buttons(b, n=len(lights)) == lights:
                     part1 += len(b)
                     break
+            part2 += sum(solve_program(buttons, joltage))
     print('Part 1:', part1)
     print('Part 2:', part2)
